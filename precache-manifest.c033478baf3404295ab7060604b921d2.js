75c86fd3f3e3724978a5ebb5f7119a0f8dd4565e
Mustafa Bhotvawala <mustafaabbs2@gmail.com>
2023-02-05 14:16:19 +0530

    Updates

---
+++ precache-manifest.c033478baf3404295ab7060604b921d2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7817795e0f0cb051c6a58a4278e42e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e3ebdd4a68896f8b081",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "eb78492a37fd8aebbc5f",
+    "url": "/static/css/main.3a2573f1.chunk.css"
+  },
+  {
+    "revision": "8e3ebdd4a68896f8b081",
+    "url": "/static/js/2.9322f4d5.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.9322f4d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb78492a37fd8aebbc5f",
+    "url": "/static/js/main.35c276eb.chunk.js"
+  },
+  {
+    "revision": "2e7e6a13be09944904b1",
+    "url": "/static/js/runtime-main.906d5e0f.js"
+  },
+  {
+    "revision": "6e5943b946ce73abf48ee4ef7f5a2341",
+    "url": "/static/media/about.6e5943b9.jpg"
+  },
+  {
+    "revision": "90e90cf325b87d917055b1c2527fafad",
+    "url": "/static/media/about.90e90cf3.jpg"
+  },
+  {
+    "revision": "c9999eaecf6b9576781508ebbe29ffb2",
+    "url": "/static/media/contact.c9999eae.jpg"
+  },
+  {
+    "revision": "b74bc4fbaebad5b6f3fe706b2e8cd841",
+    "url": "/static/media/education.b74bc4fb.jpg"
+  },
+  {
+    "revision": "ae0b11e85b440afc4aab29e25d7067c0",
+    "url": "/static/media/experience.ae0b11e8.jpg"
+  },
+  {
+    "revision": "c65235704397c72893a00879a82b38a7",
+    "url": "/static/media/firefox.c6523570.png"
+  },
+  {
+    "revision": "f51c37483a93e8e69ccadd11fb7f6b61",
+    "url": "/static/media/genesys.f51c3748.png"
+  },
+  {
+    "revision": "e37992f9dbc8a0675d12d2e22071dc7c",
+    "url": "/static/media/gm-hack-ai.e37992f9.png"
+  },
+  {
+    "revision": "a1d353255a73340bb46119002a3df88b",
+    "url": "/static/media/hackathon.a1d35325.jpg"
+  },
+  {
+    "revision": "e65b04605486db2b22dfca278178832e",
+    "url": "/static/media/hacking-good.e65b0460.png"
+  },
+  {
+    "revision": "2893107735dc7eaad332e8a95cefd416",
+    "url": "/static/media/intro.28931077.jpg"
+  },
+  {
+    "revision": "304958adf91d9ae94f56607bbab2d2bb",
+    "url": "/static/media/open-ideo.304958ad.png"
+  },
+  {
+    "revision": "5b05e611bf8706771043b3b7d18f2cb7",
+    "url": "/static/media/opensource.5b05e611.jpg"
+  },
+  {
+    "revision": "ac26bd3d87bc36645a881f162b5ab769",
+    "url": "/static/media/osce-days.ac26bd3d.png"
+  },
+  {
+    "revision": "24d09fc2ddd9ba137c76a3afc8730bd3",
+    "url": "/static/media/rbc-ngi.24d09fc2.png"
+  },
+  {
+    "revision": "51ff19f68b6360753c4979ede0c350ea",
+    "url": "/static/media/tensorflow.51ff19f6.png"
+  },
+  {
+    "revision": "7ec3ae64b0adada4aab849a5d9d604da",
+    "url": "/static/media/tohacks.7ec3ae64.png"
+  },
+  {
+    "revision": "40d4e5477752c29d5735ea383b378bdc",
+    "url": "/static/media/tunnelbear.40d4e547.png"
+  },
+  {
+    "revision": "71b8502b1846415929bfbc4e900805a3",
+    "url": "/static/media/twitter.71b8502b.png"
+  }
+]);
