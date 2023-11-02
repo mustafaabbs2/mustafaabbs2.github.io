1fb26d52f29545d464038eccaa6dba5f95a894c0
Mustafa <mustafaabbs2@gmail.com>
2023-11-02 10:54:02 +0530

    changes

--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import { Container, Col, Row } from 'reactstrap';
+import React, { useState } from 'react';
+// import { Document, Page } from 'react-pdf';
 
-import './styles.scss';
+const CV = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-export default Education;
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <h2>My Resume</h2>
+      <Document
+        file="your-resume.pdf" // Replace with the path to your PDF resume file
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+};
+
+export default CV;
